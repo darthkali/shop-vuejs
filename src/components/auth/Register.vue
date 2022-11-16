7d8221572f2a73efcf858bc3904b38454687c655
darthkali <darthkali@outlook.com>
2022-11-16 14:23:31 +0100

    added form validation in Register Component

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="my-5 text-center offset-2 col-8">
-      <img src="@/assets/welcome.svg" class="img-fluid">
+      <img src="@/assets/welcome.svg" class="img-fluid" alt="Welcome">
     </div>
     <div class="text-center">
       <h2>Jetzt registrieren</h2>
@@ -12,12 +12,15 @@
       </p>
     </div>
 
-    <Form @submit="submitData">
+    <Form @submit="submitData" :validation-schema="schema" v-slot="{errors}">
 
       <div class="form-row">
         <div class=" form-group col-md-8 offset-2">
           <label for="email"><strong>Email Adresse</strong></label>
           <Field as="input" name="email" type="email" class="form-control" id="email" placeholder="Email"/>
+          <small class="text-danger" v-if="errors.email">
+            {{ errors.email }}
+          </small>
         </div>
       </div>
 
@@ -25,13 +28,20 @@
         <div class=" form-group col-md-8 offset-2">
           <label for="password"><strong>Passwort</strong></label>
           <Field as="input" name="password" type="password" class="form-control" id="password" placeholder="Password"/>
+          <small class="text-danger" v-if="errors.password">
+            {{ errors.password }}
+          </small>
         </div>
       </div>
 
       <div class="form-row">
         <div class=" form-group col-md-8 offset-2">
           <label for="confirmPassword"><strong>Passwort wiederholen</strong></label>
-          <Field as="input" name="confirmPassword" type="password" class="form-control" id="confirmPassword" placeholder="Password"/>
+          <Field as="input" name="confirmPassword" type="password" class="form-control" id="confirmPassword"
+                 placeholder="Password"/>
+          <small class="text-danger" v-if="errors.confirmPassword">
+            {{ errors.confirmPassword }}
+          </small>
         </div>
       </div>
 
@@ -50,12 +60,33 @@
 
 <script>
 import {Form, Field} from "vee-validate";
+import * as yup from "yup";
 
 export default {
   name: "Register",
   components: {
     Form,
     Field
+  },
+  data() {
+    const schema = yup.object().shape({
+      email: yup
+          .string()
+          .required("Email Adresse wird benötigt")
+          .trim()
+          .email(),
+      password: yup
+          .string()
+          .required("Ein Password wird benötigt")
+          .min(6, "Das Passwort muss mindestens 6 Zeichen lang sein")
+          .trim(),
+      confirmPassword: yup
+          .string().required("Bitte wiederholen Sie Ihr Passwort")
+          .oneOf([yup.ref("password")], "Passwörter stimmen nicht überein")
+    })
+    return {
+      schema,
+    }
   },
   methods: {
     submitData(values) {
