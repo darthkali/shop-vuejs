08d3fcdec28149293f22797b3fd849150894cdd6
darthkali <darthkali@outlook.com>
2022-11-16 16:34:05 +0100

    added login functionality

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -12,6 +12,10 @@
       </p>
     </div>
 
+    <div class="alert alert-danger col-md-8 offset-2" v-if="error">
+      {{ errorDisplayText}}
+    </div>
+
     <Form @submit="submitData" :validation-schema="schema" v-slot="{errors}">
 
       <div class="form-row">
@@ -38,7 +42,10 @@
       <div class="form-row mt-3">
         <div class=" form-group col-md-8 offset-2">
           <div class="d-grid">
-            <button class="btn bg-vue">Einloggen</button>
+            <button class="btn bg-vue">
+              <span v-if="!isLoading">Einloggen</span>
+              <span v-else class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            </button>
           </div>
         </div>
       </div>
@@ -51,6 +58,7 @@
 <script>
 import {Form, Field} from "vee-validate";
 import * as yup from "yup";
+import axios from "axios";
 
 export default {
   name: "Login",
@@ -78,16 +86,50 @@ export default {
     })
     return {
       schema,
+      apiKey: process.env.VUE_APP_FIREBASE_WEB_API_KEY,
+      error: "",
+      isLoading: false
     }
-  }
-  ,
+  },
+  computed:{
+    errorDisplayText(){
+      if(this.error){
+        if(this.error.includes("INVALID_PASSWORD")){
+          return "Das Passwort ist nicht gültig"
+        } else if(this.error.includes("EMAIL_NOT_FOUND")){
+          return "Die Email Adresse ist nicht gültig"
+        }
+        return "Es ist ein unbekannter Fehler aufgetreten"
+      }
+      return ""
+    }
+  },
   methods: {
-    submitData(values) {
-      console.log(values)
-    },
     changeComponent(componentName) {
       this.$emit('change-component', {componentName})
-    }
+    },
+    submitData(values) {
+      this.isLoading = true;
+      this.error = "";
+      const signinDO = {
+        email: values.email,
+        password: values.password,
+        returnSecureToken: true
+      }
+      axios.post(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.apiKey,
+          signinDO
+      ).then(response => {
+        console.log(response);
+        this.isLoading = false;
+      }).catch(error => {
+        // https://firebase.google.com/docs/reference/rest/auth#section-error-format
+        this.error = error.response.data.error.message;
+        this.isLoading = false;
+      })
+
+
+    },
   }
 }
 </script>
