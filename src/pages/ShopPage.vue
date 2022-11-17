683f9a028fc48a98b4a49ed21117e64896d48ea2
darthkali <darthkali@outlook.com>
2022-11-17 16:20:56 +0100

    added ProductListItem

--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -1,18 +1,24 @@
 <template>
   <TheShopLayout>
   <template #default>
-    <h2>Hello</h2>
+    <div class="row">
+      <div class="col-3 m-3" v-for="i in 3" :key="i">
+        <ProductListItem/>
+      </div>
+    </div>
   </template>
   </TheShopLayout>
 </template>
 
 <script>
 import TheShopLayout from "@/layouts/TheShopLayout";
+import ProductListItem from "@/components/shop/ProductListItem";
 
 export default {
   name: "ShopPage",
   components: {
-    TheShopLayout
+    TheShopLayout,
+    ProductListItem
   }
 }
 </script>
