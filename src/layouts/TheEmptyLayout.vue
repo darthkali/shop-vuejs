07aa8292426b74e60b43ffc9815f0ca83447dc83
darthkali <darthkali@outlook.com>
2022-12-07 18:11:51 +0100

    added EmptyLayout

---
+++ src/layouts/TheEmptyLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheEmptyLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
